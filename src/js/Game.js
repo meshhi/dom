b5574a2af58fe5c9874d06231376fb0810d79095
Michael <yumythug@yandex.ru>
2023-09-25 22:11:52 +0300

    gamed

---
+++ src/js/Game.js
@@ -0,0 +1,32 @@
+import './Game.css';
+import goblin from './Goblin';
+
+export default class Game {
+    start() {
+        document.querySelector('body').appendChild(this.board);
+        goblin.jumpOverBoard(this.board);
+        this.board.addEventListener('click', (e) => {
+            if (e.target.classList.contains('goblin')) {
+                goblin.beatUp();
+                const win = document.createElement('div');
+                win.classList.add('win');
+                document.body.appendChild(win);
+                win.innerHTML = 'You win!';
+                win.addEventListener('click', (e) => {
+                    e.preventDefault();
+                }, true);
+            }
+        })
+    }
+
+    initBoard() {
+        console.log(this)
+        this.board = document.createElement('div');
+        this.board.classList.add('board');
+        for (let i = 0; i < 16; i++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            this.board.appendChild(cell);
+        }
+    }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,5 @@
 // TODO: write code here
-
-// comment this to pass build
-const unusedVariable = "variable";
-
-// for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
-}
-
-console.log("app.js included");
+import Game from "./Game";
+const game = new Game();
+game.initBoard();
+game.start();
