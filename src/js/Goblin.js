20e488a04ea3532383dd2cb7c82991020657aa75
Michael <yumythug@yandex.ru>
2023-09-25 22:53:55 +0300

    linter

--- src/js/Goblin.js
+++ src/js/Goblin.js
@@ -1,34 +1,38 @@
-import * as url from '../img/goblin.png';
-import './Goblin.css';
+import * as url from "../img/goblin.png";
+import "./Goblin.css";
 
 export class Goblin {
-    constructor() {
-        this.goblin = document.createElement('img')
-        this.goblin.src = url.default;
-        this.goblin.alt = 'Goblin Logo';
-        this.goblin.classList.add('goblin');
-    }
+  constructor() {
+    this.goblin = document.createElement("img");
+    this.goblin.src = url.default;
+    this.goblin.alt = "Goblin Logo";
+    this.goblin.classList.add("goblin");
+  }
 
-    jumpOverBoard = (board) => {
-        this.isJumping = setInterval(() => {
-            try {
-                this.goblin.remove();
-                let newCurrentPosition = Math.floor(Math.random() * board.children.length);
-                while (newCurrentPosition == this.currentPosition) {
-                    newCurrentPosition = Math.floor(Math.random() * board.children.length);
-                }
-                this.currentPosition = newCurrentPosition;
-                board.children[this.currentPosition].append(this.goblin);
-            } catch(e) {
-                console.error(e);
-            }
-        }, 1000)
-    }
+  jumpOverBoard = (board) => {
+    this.isJumping = setInterval(() => {
+      try {
+        this.goblin.remove();
+        let newCurrentPosition = Math.floor(
+          Math.random() * board.children.length
+        );
+        while (newCurrentPosition == this.currentPosition) {
+          newCurrentPosition = Math.floor(
+            Math.random() * board.children.length
+          );
+        }
+        this.currentPosition = newCurrentPosition;
+        board.children[this.currentPosition].append(this.goblin);
+      } catch (e) {
+        console.error(e);
+      }
+    }, 1000);
+  };
 
-    beatUp = () => {
-        clearInterval(this.isJumping);
-        console.log('Goblin beat up!');
-    }
+  beatUp = () => {
+    clearInterval(this.isJumping);
+    console.log("Goblin beat up!");
+  };
 }
 
 export default new Goblin();
