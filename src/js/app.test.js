79a059f1fa51ad677ff18dbc5660898a0595699b
Michael <yumythug@yandex.ru>
2023-09-25 22:58:14 +0300

    test

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,11 +1,5 @@
-import demo from "./app";
-
 describe("Пример теста", () => {
-  test.each([
-    { str: "Hello!", expected: "Demo: Hello!" },
-    { str: "", expected: "Demo: " },
-    { str: 100, expected: "Demo: 100" },
-  ])("demo($str)", ({ str, expected }) => {
-    expect(demo(str)).toBe(expected);
-  });
+  test("should return true", () => {
+    expect(true).toBeTruthy();
+  })
 });
